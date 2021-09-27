72ea2b841cd258047f71113288c402fc1c375bea
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-27 21:30:43 +0200

    new comit

---
+++ project3-colors/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {Helmet} from "react-helmet";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Put somme colors in your digital life
+        </h1>
+    
+      
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
